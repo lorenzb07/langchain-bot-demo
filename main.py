f441e488e44af22cd7f2b882d349912af33f9956
Lorenzo <lorenzobolognesi90@gmail.com>
2024-11-22 18:39:27 +0100

    feat: add streamlit

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import sys
 
-from bot import run_agent
+from bot.agents import init_agent, run_agent
 
 
 def main():
-    model = sys.argv[1]
-    question = sys.argv[2]
+    """
+    Main function to run the agent with the specified model and question.
+
+    This function retrieves the model name and question from the
+    command line arguments and calls the `run_agent` function
+    if a question is provided.
+
+    Command line arguments:
+    model_name (str): The name of the model to use.
+    question (str): The question to ask the model.
+    """
+    provider = sys.argv[1]
+    model_name = sys.argv[2]
+    question = sys.argv[3]
 
     if question != "":
-        run_agent(
-            model_name=model,
+        agent_executor = init_agent(provider, model_name)
+
+        answer = run_agent(
+            agent_executor=agent_executor,
             question=question,
         )
+        print(answer)
+
 
 if __name__ == "__main__":
     main()
